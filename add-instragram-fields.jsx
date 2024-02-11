a14a8ffa8593dfa33298d9cb76f78170c60b0aa8
Sergei Reshetniak <reshetniaksv@gmail.com>
2024-02-11 19:28:13 +0100

    Add script to make 4x5 photos for instagram with fields

---
+++ add-instragram-fields.jsx
@@ -0,0 +1,49 @@
+﻿// Script to resize images for Instagram
+
+/*
+
+// BEGIN__HARVEST_EXCEPTION_ZSTRING
+
+<javascriptresource>
+<name>Copyright</name>
+<about>Sample script for adding a copyright note.</about>
+<menu>filter</menu>
+<category>Tutorial</category>
+<enableinfo>true</enableinfo>
+</javascriptresource>
+
+// END__HARVEST_EXCEPTION_ZSTRING
+
+*/
+
+const OUTPUT_WIDTH = 1800;
+const OUTPUT_HEIGTH = 2250;
+
+function add4x5Fields() {
+    app.preferences.rulerUnits = Units.PIXELS;
+    app.preferences.typeUnits = TypeUnits.PIXELS;
+
+    var doc = app.activeDocument;
+
+    //doc.mergeVisibleLayers();
+
+    var newWidth, newHeigth;
+    if (doc.width * 5 / 4 > doc.height) { 
+        newWidth = doc.width;
+        newHeigth = doc.width * 5 / 4;
+    } else {
+        newWidth = doc.height * 4 / 5;
+        newHeigth = doc.height;
+    }
+    
+    doc.resizeCanvas(newWidth, newHeigth, AnchorPosition.MIDDLECENTER);
+
+    doc.resizeImage(
+        OUTPUT_WIDTH,
+        OUTPUT_HEIGTH,
+        OUTPUT_WIDTH * OUTPUT_HEIGTH,
+        resampleMethod=ResampleMethod.BICUBICSMOOTHER,    
+    );
+}
+
+add4x5Fields()
